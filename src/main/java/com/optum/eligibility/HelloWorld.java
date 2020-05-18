c82353700b0724118e2eb5a997a03edcac6e2efd
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2020-05-18 14:29:37 -0400

    intial commit

---
+++ src/main/java/com/optum/eligibility/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.optum.eligibility;
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        int i = 10;
+        while (i > 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
+
