302d6236e303c295c0301a71245175dcf010e0e6
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2020-05-18 14:31:43 -0400

    Update HelloWorld.java

--- src/main/java/com/optum/eligibility/HelloWorld.java
+++ src/main/java/com/optum/eligibility/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String args[]) {
         int i = 10;
         while (i > 1) {
-            System.out.println(i);
+            System.out.println("Rani---"+ i);
             i--;
         }
     }
