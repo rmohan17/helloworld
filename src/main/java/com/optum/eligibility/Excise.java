154132071a2a6394a5a47feb8a87c73d73a45064
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2020-05-27 12:07:12 -0400

    Update Excise.java

--- src/main/java/com/optum/eligibility/Excise.java
+++ src/main/java/com/optum/eligibility/Excise.java
@@ -3,8 +3,8 @@ package com.optum.eligibility;
 public class Excise {
     public static void main(String[] args) {
         //(a+b)squre = ((a*a)+2ab+(b*b))
-        int  a = 2;
-        int b =  1;
+        int  a = 5;
+        int b =  10;
         int value = (a*a + 2*a*b + b*b);
         System.out.println("value of the equation is = "+ value);
     }
