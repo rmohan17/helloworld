da7ae2a257e55516f3b1d2afb4bb66936c49874d
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2020-05-27 12:00:46 -0400

    Excise class

---
+++ src/main/java/com/optum/eligibility/Excise.java
@@ -0,0 +1,11 @@
+package com.optum.eligibility;
+
+public class Excise {
+    public static void main(String[] args) {
+        //(a+b)squre = ((a*a)+2ab+(b*b))
+        int  a = 2;
+        int b =  1;
+        int value = (a*a + 2*a*b + b*b);
+        System.out.println("value of the equation is = "+ value);
+    }
+}
--- src/main/java/com/optum/eligibility/TypeCast.java
+++ src/main/java/com/optum/eligibility/TypeCast.java
@@ -2,8 +2,12 @@ package com.optum.eligibility;
 
 public class TypeCast {
     public static void main(String[] args) {
-        short x=6;
-        int y=x;
-        System.out.println();
+        long x= 500;
+       float y = (float)x;
+        System.out.println(y);
+        float v1 = 10.0f;
+        double v2 = 20.0;
+        System.out.println(v2);
+
     }
 }
