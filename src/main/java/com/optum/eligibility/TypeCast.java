c77d297af82d4239577a278975c8dbfb52b8d535
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2020-05-27 07:04:03 -0400

    added TypeCast class

--- src/main/java/com/optum/eligibility/TypeCast.java
+++ src/main/java/com/optum/eligibility/TypeCast.java
@@ -1,4 +1,9 @@
 package com.optum.eligibility;
 
 public class TypeCast {
+    public static void main(String[] args) {
+        short x=6;
+        int y=x;
+        System.out.println();
+    }
 }
