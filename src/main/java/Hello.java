406b81c7786a375cdefc91a4554b190c35982809
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2020-05-27 18:42:03 -0400

    Hello

---
+++ src/main/java/Hello.java
@@ -0,0 +1,46 @@
+import java.math.BigDecimal;
+
+public class Hello {
+    public static void main(String[] args) {
+      /*  int value1 = 5/2;
+        float value2 = 10f/9f;
+        double value3 = 10d/9d;
+        System.out.println("value1 = "+value1);
+        System.out.println("value2 = "+value2);
+        System.out.println("value3 = "+value3);
+        */
+       /* int marker = 200;
+        double percentage = marker * 0.40f;
+        System.out.println("percentage =" +percentage);
+         */
+        /*boolean var= false;
+        System.out.println(var);
+         */
+        /*char var = '\u2764';
+             char var1= '\u2764';
+        System.out.println(var);
+        System.out.println(var1);
+         */
+       /* double x= 1.05;
+        double y= 2.55;
+        BigDecimal d1 = new BigDecimal("2.05");
+        BigDecimal d2 = new BigDecimal("1 .55");
+        System.out.println(d1.subtract(d2));
+       */
+/*
+        int  a= 10;
+        String b= "20";
+        System.out.println(a+20);
+        String var =new String("hello");
+        System.out.println(var);
+        
+ */
+
+        int x = 3;
+        System.out.println(--x);
+        System.out.println(x);
+
+
+
+    }
+}
