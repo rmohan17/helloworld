81da946f21256bc78f3327672267504850f81a6e
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2020-06-03 16:53:12 -0400

    change value

--- src/main/java/com/optum/eligibility/Demo.java
+++ src/main/java/com/optum/eligibility/Demo.java
@@ -5,7 +5,7 @@ import com.optum.eligibility.methods.Calculate;
 public class Demo {
     public static void main(String[] args) {
         Calculate prime = new Calculate();
-        if (prime.isprime(23)) {
+        if (prime.isprime(24)) {
             System.out.println("number passed is prime");
         } else {
             System.out.println("number passed is not prime");
